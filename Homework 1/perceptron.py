3f849f639bdcb42e85a98a5361b51eaaa8026ab5
Ana Mestre <ana.gabriela.mestre@est.fib.upc.edu>
2019-10-30 10:02:48 +0100

    Final version

--- Homework 1/perceptron.py
+++ Homework 1/perceptron.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 17 20:14:49 2019
+Created on Thu Oct 16 20:14:49 2019
 
 @author: Ana
 """
 import random
 import matplotlib.pyplot as plt
+import time
 
 """
 Class Point(N)
-coords = [x_0, x_1, ... x_N-1]
+coords = [x_0, x_1, ... x_N-1], with N >= 2
 label = our target function
 """
 class Point:
@@ -21,11 +22,13 @@ class Point:
     def __init__(self, N):
         #self.x = random.uniform(-20,20)
         #self.y = random.uniform(-20,20)
-        self.coords = [random.uniform(-20, 20) for _ in range(N)]
-        if self.coords[1] >= self.coords[0]:
+        self.coords = [random.uniform(-1, 1) for _ in range(N)]
+        if self.coords[0] > self.coords[1]:
             self.label = 1
         else:
             self.label = -1
+
+#######################################
 
 def guess(inputs):
     sum_ = 0
@@ -57,7 +60,7 @@ def print_points(points):
         else:
             color = "r"
         plt.scatter(*pt, color=color)
-    plt.plot([-20, 20], [-20, 20], "-b")
+    plt.plot([-dims, dims], [-dims, dims], "-b") # line 
     plt.show()
  
 # Looking for misclassifications
@@ -71,25 +74,37 @@ def check_points(points):
     return mis_classification
 
 
+
+# Initializations
+# ###############
 size = 1000
-N = 10
-weights = [random.uniform(-1,1) for _ in range(N)]
+dims = 1
+N = 2
+random.seed(36) # Changed for every test
+weights = [random.randint(-1,1) for _ in range(N)]
 points = [Point(N) for _ in range(size)]
 learning_rate = 0.1
 
+
+
 # Print initial points
-print("\n Dataset of size:", size)
+# ####################
+#print("\n Dataset of size:", size)
 #inputs = [(p.x, p.y) for p in points]
 inputs = [p.coords for p in points]
-#plt.scatter(*zip(*inputs))
-#plt.plot([-20, 20], [-20, 20], "-b")
-#plt.show()
 
-i = 1
+# Comment these three lines for N = 10
+plt.scatter(*zip(*inputs))
+plt.plot([-dims, dims], [-dims, dims], "-b") # line 
+plt.show()
+# ##############################################
+
+start_time = time.time()
+i = 0
 while True:
     i += 1
-    print("\n Iteration number: " + str(i))
-    #print_points(points)
+    print("\nIteration number: " + str(i))
+    print_points(points) # Comment theis line for N = 10
     mis_class = check_points(points)
     if len(mis_class) == 0:
         break
@@ -98,3 +113,11 @@ while True:
         #inp = [point.x, point.y]
         inp = point.coords
         train(inp, point.label)
+print("\nTime needed for execution:")
+print(time.time() - start_time)
+
+random.seed(37) # Changed for every test
+testing_points = [Point(N) for _ in range(size)]
+num_errors = len(check_points(testing_points))
+accuracy = 100 * (size - num_errors) / size
+print ("Accuracy: " + str(accuracy))
