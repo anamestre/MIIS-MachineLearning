74037aa3afcc91f77771f5ac8908dd41da3df428
Ana Mestre <ana.gabriela.mestre@est.fib.upc.edu>
2019-11-22 08:23:39 +0100

    is this working?omg - gotta check those e_out values, though

--- Homework 2/homework.py
+++ Homework 2/homework.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import statistics
 
 def initialize():
     Qf2, Qf10 = 2, 10
@@ -20,7 +21,7 @@ def initialize():
     X = np.random.uniform(-1, 1, N)
     X.sort()
     #_, std = norm.fit(X)
-    sigma = 0.3
+    sigma = 0.2
     epsilon = np.random.standard_normal(N)
     return Qf2, Qf10, N, sigma, X, epsilon
 
@@ -47,19 +48,83 @@ def plot_function(vals, f, label = " ", color = "-r"):
 # Y_n = f(x_n) + sigma * epsilon_n
 # f(x) = sum(from q = 0 to Qf) (a_q) * L_q(x)
 def get_y(fx, sigma, epsilon):
-    y = [fx[i] + ((sigma**2) * epsilon[i]) for i in range(len(fx))]
+    #y = [fx + ((sigma) * epsilon[i]) for i in range(Qx)]
+    y = fx + sigma * epsilon
+    # y = [fun_x(x[i], Qf, aqs) + ((sigma) * epsilon[i]) for i in range(Qf)]
     return y
 
-def polRegression(x, y, Qf):
+def polRegression(x, y, Qf, color = "r", title=""):
+    x_reshape = x.reshape(-1, 1)
     poly_feat = PolynomialFeatures(degree = Qf, include_bias = True)
     lin_reg = LinearRegression()
-    x_ = poly_feat.fit_transform(x.reshape(-1, 1))
+    x_ = poly_feat.fit_transform(x_reshape)
     lin_reg.fit(x_, y)
     y_pred = lin_reg.predict(x_)
-    plt.scatter(x, y)
-    plt.plot(x, y_pred, c = "r")
+    plt.scatter(x_reshape, y, label = "points")
+    plt.plot(x, y_pred, c = color, label = "h" + str(Qf))
+    if title != "":
+        plt.title(title, fontsize = 16)
+    plt.legend()
+    plt.show()
+    return x, y_pred, lin_reg, poly_feat
+    
+def polRegression_models(x, Qf, sig, eps, n):
+    aq = np.random.standard_normal(Qf + 1)
+    norm_aq = norm_aqs(aq, Qf)
+    
+    g = fun_x(x, Qf, norm_aq)
+    Y = get_y(g, sig, eps)
+    
+    title = "N = " + str(N) + ", Sigma = " + str(sig) + ", Qf = " + str(Qf)
+    title2 = "H2 - " + title
+    title10 = "H10 - " + title
+
+    x2, y_pred2, lin_reg2, poly_feat2 = polRegression(x, Y, 2, "r", title2)
+    x10, y_pred10, lin_reg10, poly_feat10 = polRegression(x, Y, 10, "g", title10)
+    
+    # E_out:
+    x_ = np.random.uniform(-1, 1, n)
+    x_.sort()
+    y_ = fun_x(x_, Qf, norm_aq)
+    x_reshape = x_.reshape(-1, 1)
+    y2_hat = lin_reg2.predict(poly_feat2.fit_transform(x_reshape))
+    y10_hat = lin_reg10.predict(poly_feat10.fit_transform(x_reshape))
+    
+    plt.scatter(x_, y_, label = "points")
+    plt.title(title, fontsize = 16)
+    plt.plot(x_, y2_hat, c = "r", label = "h2")
+    plt.plot(x_, y10_hat, c = "g", label = "h10")
+    plt.legend()
     plt.show()
     
+    E_out2  = np.power(y_ - y2_hat, 2).mean()
+    E_out10 = np.power(y_ - y10_hat, 2).mean()
+    
+    return E_out2, E_out10
+    
+
+def print_different_errors():
+    Ns = np.arange(50, 121, 50)
+    sigmas = np.arange(0.5, 1.05, 0.3)
+    Qfs = np.arange(5, 16, 5)
+    it = 1
+    for n in Ns:
+        for qf in Qfs:
+            for sig in sigmas:
+                err2_total, err10_total = [], []
+                for i in range(it):
+                    x = np.random.uniform(-1, 1, n)
+                    x.sort()
+                    eps = np.random.standard_normal(n)
+                    err2, err10 = polRegression_models(x, qf, sig, eps, n)
+                    err2_total.append(err2)
+                    err10_total.append(err10)
+                err2_mean = statistics.mean(err2_total)
+                err10_mean = statistics.mean(err10_total)
+                overfit = err10_mean - err2_mean
+                print("For N =", n, "qf =", qf, "sigma =", sig, "error2 =", err2_mean, "error10=", err10_mean, "overfit =", overfit)
+                
+                    
 
 Qf2, Qf10, N, sigma, X, epsilon = initialize()
 
@@ -69,17 +134,13 @@ norm_aq2 = norm_aqs(aq2, Qf2)
 aq10 = np.random.standard_normal(Qf10 + 1)
 norm_aq10 = norm_aqs(aq10, Qf10)
 
-# ... rescaling so E[f^2] = 1
-#norm_aq2 = preprocessing.normalize([aq2] , norm = 'l2')
-#norm_aq10 = preprocessing.normalize([aq10] , norm = 'l2')
-
 g2 = fun_x(X, Qf2, norm_aq2) # H2
 g10 = fun_x(X, Qf10, norm_aq10) # H10
-#plot_function(X, g2, "H2", "-r")
-#plot_function(X, g10, "H10", "-b")
 
 Y2 = get_y(g2, sigma, epsilon)
 Y10 = get_y(g10, sigma, epsilon)
 
-polRegression(X, Y2, Qf2)
-polRegression(X, Y10, Qf10)
+polRegression(X, Y2, Qf2, "r")
+polRegression(X, Y10, Qf10, "g")
+
+print_different_errors()
